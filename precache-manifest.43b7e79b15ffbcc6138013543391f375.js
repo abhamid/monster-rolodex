8513ff1e1ce371f4e9763c8ee79605568b01896a
Abdul Hamid <abdul.hamid.kol8@gmail.com>
2019-07-05 11:11:28 +0530

    Updates

---
+++ precache-manifest.43b7e79b15ffbcc6138013543391f375.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226af6fb53821879fa4cbb05b9f616b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ef333314a122712a4a99",
+    "url": "/monsters-rolodex/static/css/main.8a5ae6fc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ef333314a122712a4a99",
+    "url": "/monsters-rolodex/static/js/main.74524c1c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
